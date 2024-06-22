61c528a22fb14c988ffbfc2e5b3a340876a1b03e
Abderrahmane <cszamasu@gmail.com>
2024-06-22 15:21:00 +0100

    OP

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "Nosquare",
-            template: "../dist/index.html"
+            template: "./dist/index.html"
         })
     ]
 };
